ffb174a2468c13b29db1c3be740e40f8ed66711e
Kirill Lossev <kirill.lossev@mail.utoronto.ca>
2023-09-11 15:03:22 -0400

    Add versioning to components

--- obstacle_handling/run_obstacle_handling.py
+++ obstacle_handling/run_obstacle_handling.py
@@ -6,10 +6,27 @@ if not os.environ["MQ_SERVER"]:
     print("The address of the Message Queue server is not specified. Set the MQ_SERVER environment variable.")
     exit()
 
+if not os.environ["VERSION"]:
+    print("The component version to run is not specified. Set the VERSION environment variable.")
+    exit()
+
+
 (mq_host, mq_port) = parse_hostport(os.environ["MQ_SERVER"])
 
-def is_distance_safe(distance: float):
+def is_distance_safe_v1(distance: float):
     return distance >= 50 or distance == 0 # in metres
+
+def is_distance_safe_v2(distance: float):
+    return distance >= 75 or distance == 0 # in metres
+
+is_distance_safe = None
+if os.environ["VERSION"] == "1":
+    is_distance_safe = is_distance_safe_v1
+elif os.environ["VERSION"] == "2":
+    is_distance_safe = is_distance_safe_v2
+else:
+    print("Invalid version.")
+    exit()
 
 def process_message(channel, method, properties, body):
     distance = float(body)
